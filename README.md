f805af7a159619724434e69503b62d79ec13db06
Veronika <n.kanchura.dodo@gmail.com>
2021-05-21 23:06:09 +0400

    add test Zayavka

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/ysnx7tyc0w3x4gap/branch/main?svg=true)](https://ci.appveyor.com/project/Veronika-ui/aqa3-selenium/branch/main)
 
 
---
+++ src/test/java/ru/netology/selenium/CallbackTest.java
@@ -0,0 +1,123 @@
+package ru.netology.selenium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.By.*;
+
+class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vyakovleva\\Desktop\\Прил\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
+    @Test
+    void shouldTest() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(cssSelector("[class='input__control'][type='text']")).sendKeys("Пупкин Вася");
+        driver.findElement(cssSelector("[class='input__control'][type='tel']")).sendKeys("+79967466215");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", message.trim());
+
+
+    }
+
+
+    @Test
+    void shouldTestError1() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(cssSelector("[class='input__control'][type='text']")).sendKeys("");
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", message.trim());
+
+
+    }
+
+    @Test
+    void shouldTestErrorName() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(cssSelector("[class='input__control'][type='text']")).sendKeys("Nika - Mironova");
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", message.trim());
+
+    }
+
+    @Test
+    void shouldTestErorrNumber() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(cssSelector("[class='input__control'][type='text']")).sendKeys("Пупкин Вася");
+        driver.findElement(cssSelector("[class='input__control'][type='tel']")).sendKeys("+79");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input_invalid .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", message.trim());
+
+    }
+
+
+    @Test
+    void shouldTestEmptyField() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(cssSelector("[class='input__control'][type='text']")).sendKeys("");
+        driver.findElement(cssSelector("[class='input__control'][type='tel']")).sendKeys("");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", message.trim());
+
+
+    }
+    @Test
+    void shouldTestEmpty() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(cssSelector("[class='input__control'][type='text']")).sendKeys("");
+        driver.findElement(cssSelector("[class='input__control'][type='tel']")).sendKeys("+79967466215");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", message.trim());
+
+
+    }
+
+
+    @Test
+    void shouldTestUserAgreement() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(cssSelector("[class='input__control'][type='text']")).sendKeys("");
+        driver.findElement(cssSelector("[class='input__control'][type='tel']")).sendKeys("+79967466215");
+        driver.findElement(cssSelector(".checkbox__box")).click();
+        driver.findElement(cssSelector("button")).click();
+        String message = driver.findElement(cssSelector(".checkbox__text")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", message.trim());
+
+
+    }
+}
