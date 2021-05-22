b69553053800b021c3d24f35e030647519e270bd
Veronika <n.kanchura.dodo@gmail.com>
2021-05-22 23:26:34 +0400

    update YML

--- src/test/java/ru/netology/selenium/CallbackTest.java
+++ src/test/java/ru/netology/selenium/CallbackTest.java
@@ -32,7 +32,7 @@ class CallbackTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
 
